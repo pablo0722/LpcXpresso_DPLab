86225bfa8428cae2fa7498eec0ff3b0110b815e9
Pablo0722 <pablod_angelo@est.frba.utn.edu.ar>
2016-06-10 22:16:55 -0300

    actualizado toLPCandback para que haga loopback de una rampa (aun no funciona 100%)

--- Matlab/toLPCandback.m
+++ Matlab/toLPCandback.m
@@ -16,10 +16,11 @@ signal=signal*(2^32)*0.99;
 % para ver el puerto, en la consola dmesg
 
 srl = serial('/dev/ttyUSB0','BaudRate',9600,'DataBits',8,'StopBits',1);
+srl.InputBufferSize = 2048*4;
 fopen(srl);
 
 for h=1:size(signal,2)
-    fwrite(srl,int32(signal(h)),'int32','sync');
+    fwrite(srl,(h),'int32','sync');
     h
 end
 % plot(signal);
@@ -34,7 +35,7 @@ FractLength = 31 ;
 q = quantizer( 'Mode', 'fixed','Format' ,[WordLength FractLength],'OverflowMode', 'Saturate','RoundMode','floor');
     
 display('ahi vienen los indios')
-resultados=fread(srl);
+[resultados, count, msg]=fread(srl, 2048, 'int32');
 Y = typecast(resultados, 'int32'); % agrupo de a 4 bytes
 Ym= dec2hex(Y);
 Ym= hex2num(q,Ym);
